c18985191c3c768309f6b4278dbf5f8b889c9151
lalifeier <lalifeier@gmail.com>
2021-06-14 22:01:42 +0800

    feat: init

--- src/directive/modules/lazy.ts
+++ src/directive/modules/lazy.ts
@@ -5,18 +5,12 @@ const lazy = new Lazy()
 
 export default {
   mounted(el: HTMLElement, binding: DirectiveBinding<any>) {
-    console.log('mounted lazy')
-
     lazy.add(el, binding.value)
   },
   update(el: HTMLElement, binding: DirectiveBinding<any>) {
-    console.log('update lazy')
-
     lazy.update(el, binding.value)
   },
   unmounted(el: HTMLElement) {
-    console.log('unmounted lazy')
-
     lazy.remove(el)
   },
 }
