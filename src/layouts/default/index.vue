be84f7b0325474ee6cf4cc8fd6cbb0271cfa729d
lalifeier <lalifeier@gmail.com>
2021-08-15 22:59:08 +0800

    feat: init

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -5,14 +5,15 @@
     </v-navigation-drawer>
 
     <v-app-bar app>
-      <v-card>
+      <v-card class="flex-grow-1 d-flex mt-3" color="surface">
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
 
         <v-btn icon @click.stop="miniVariant = !miniVariant">
           <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
         </v-btn>
+
         <!-- <v-toolbar-title>hi</v-toolbar-title> -->
-        <v-spacer />
+        <!-- <v-spacer /> -->
       </v-card>
     </v-app-bar>
 
@@ -25,18 +26,25 @@
     </v-main>
 
     <v-footer app>
-      <div class="text-center text-body-2 font-weight-bold secondary--text text--lighten-2">
-        Admin Pro © All Rights Reserved
-      </div>
+      <v-container>
+        <div class="d-flex flex-column align-center">
+          <div class="my-1 secondary--text">
+            Copyright © 2020
+            <router-link to="#" class="text-decoration-none secondary--text text--lighten-3"
+              >Admin Pro</router-link
+            >, All rights Reserved
+          </div>
+        </div>
+      </v-container>
     </v-footer>
   </div>
 
   <Backtop> </Backtop>
 </template>
 <script setup lang="ts">
+  import { ref } from 'vue'
   import Backtop from '/@/components/Backtop'
 
-  import { ref } from 'vue'
   const drawer = ref(false)
   const miniVariant = ref(false)
 </script>
